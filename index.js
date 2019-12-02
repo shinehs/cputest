0c8452a05b11891528c8a7aa9f4154f8596a64c7
huangshuai <huangshuai@yy.com>
2019-12-02 17:49:17 +0800

    feat: add

---
+++ index.js
@@ -0,0 +1,9 @@
+let jsdata = {};
+tenJson = () => {
+  for (let i = 0; i < 10 * 1024; i++) {
+    jsdata[i] = i * parseInt(Math.random() * 100000) + "";
+  }
+};
+tenJson();
+let formatData = JSON.stringify(jsdata);
+console.log("~~~", JSON.parse(formatData));
